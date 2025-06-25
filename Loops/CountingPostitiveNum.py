fc6f7b6530f069dc069e76817509f1ff0b8b6ce5
Machahary07 <jeumachahary07@gmail.com>
2025-06-25 20:01:46 +0530

    deletion

--- Loops/CountingPostitiveNum.py
+++
@@ -1,8 +0,0 @@
-# to count the number of positive numbers in a list of numbers.
-
-numbers = [1, -2, 3, -4, 5, -6, 7, -8, 9, 10]
-
-def count_positive_numbers(numbers):
-    return len([num for num in numbers if num > 0])
-
-print(count_positive_numbers(numbers))
