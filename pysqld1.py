b2feb89ba45678e472cb28dc59c193ee232ea516
Machahary07 <jeumachahary07@gmail.com>
2025-06-26 12:25:36 +0530

    pysqlfix

--- pysqld1.py
+++ pysqld1.py
@@ -1,6 +1,4 @@
 import csv
-import sys
-sys.path.append(r'C:\Users\samsung\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\LocalCache\local-packages\Python312\site-packages')
 import mysql.connector
 
 # Connect to MySQL
